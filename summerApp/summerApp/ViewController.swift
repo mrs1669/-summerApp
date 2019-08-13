e846246bc80a74655b0692d4a28a16029eccebe2
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-08-13 20:55:10 +0900

    New project

---
+++ summerApp/summerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  summerApp
+//
+//  Created by Takumi Muraishi on 2019/08/13.
+//  Copyright © 2019 Takumi Muraishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
