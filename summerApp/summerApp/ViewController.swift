0e793fc9c56748f89d54aa753ff2a35ff6e48aa4
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-08-14 10:50:48 +0900

    add UIlabel

--- summerApp/summerApp/ViewController.swift
+++ summerApp/summerApp/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 150, width: 100, height: 100)
+        label.text = "TEST"
+        self.view.addSubview(label)
     }
 
 
