1f98addd9091e382e2c9cd29281029609d4ef38b
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-08-14 17:14:04 +0900

    少し追加

--- summerApp/summerApp/ViewController.swift
+++ summerApp/summerApp/ViewController.swift
@@ -13,10 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let label = UILabel()
-        label.frame = CGRect(x: 25, y: 11, width: 80, height: 20)
-        label.text = "■■■■"
+        
+        let label = UILabel() // ラベル"label"を宣言
+        label.frame = CGRect(x: 150, y: 300, width: 65, height: 30) // ラベルの位置、大きさ指定
+        label.text = "テキスト"
+        label.textColor = UIColor.green
+        label.backgroundColor = UIColor.orange
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 10.0
         self.view.addSubview(label)
+        self.view.backgroundColor = UIColor.cyan
     }
 
 
